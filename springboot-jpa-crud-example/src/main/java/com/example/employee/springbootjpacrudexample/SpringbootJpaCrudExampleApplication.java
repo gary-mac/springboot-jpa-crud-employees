38ec791a23946c041bd68097356bf694f46b4d35
Gary Maccabe <garymaccabe93@gmail.com>
2023-02-22 13:06:13 +0000

    Initial commit

---
+++ springboot-jpa-crud-example/src/main/java/com/example/employee/springbootjpacrudexample/SpringbootJpaCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee.springbootjpacrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaCrudExampleApplication.class, args);
+	}
+
+}
